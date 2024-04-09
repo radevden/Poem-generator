37671cdfa7b2a9537efc2f26187e5630640ca12a
radevden <radevden@gmail.com>
2024-04-09 16:20:42 +0200

    a small JS update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function typePoem(response){
 
     new Typewriter (".pPoem", {
-        strings: response.data,
+        strings: response.data.answer,
         autostart: true,
         delay: 50,
     });
@@ -11,10 +11,6 @@ function typePoem(response){
 function generatePoem(event) {
     event.preventDefault();
 
-
-    let form = document.querySelectorAll("#form");
-    form.addEventListener("submit", generatePoem);
-    
     let apiKey = "e1c36520c14f56fa74b8fob3tcc313d4";
     let prompt = "Please generate a poem for the chosen topic";
     let context = "You are a poem generator, please generate a short 2 verse poem."
@@ -22,4 +18,7 @@ function generatePoem(event) {
 
     axios.get(apiUrl).then(typePoem);
 }
+
+let form = document.querySelectorAll("#form");
+form.addEventListener("submit", generatePoem);
 
