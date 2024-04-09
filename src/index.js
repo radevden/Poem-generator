74769f555ba6d34ed9b88f4b2ff39430cc1dea6e
radevden <radevden@gmail.com>
2024-04-09 16:15:28 +0200

    JS but not working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-function generatePoem(event) {
-    event.preventDefault();
-    
-    alert("Generating a poem");
+function typePoem(response){
 
     new Typewriter (".pPoem", {
-        strings: "Type the poem here",
+        strings: response.data,
         autostart: true,
         delay: 50,
     });
+
 }
 
-let form = document.querySelectorAll("#form");
-form.addEventListener("submit", generatePoem);
+function generatePoem(event) {
+    event.preventDefault();
 
 
+    let form = document.querySelectorAll("#form");
+    form.addEventListener("submit", generatePoem);
+    
+    let apiKey = "e1c36520c14f56fa74b8fob3tcc313d4";
+    let prompt = "Please generate a poem for the chosen topic";
+    let context = "You are a poem generator, please generate a short 2 verse poem."
+    let apiUrl =  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    axios.get(apiUrl).then(typePoem);
+}
 
