dc58d31503bf16427277a3b4482441c04a9c9bed
radevden <radevden@gmail.com>
2024-04-18 11:50:54 +0200

    updated JS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-function typePoem(response){
-
-    new Typewriter (".pPoem", {
+function typePoem(response) {
+    console.log(response); // Log the entire response object
+    console.log(response.data); // Log the data property of the response object
+    new Typewriter(".pPoem", {
         strings: response.data.answer,
         autostart: true,
         delay: 50,
     });
-
 }
 
 function generatePoem(event) {
@@ -13,12 +13,11 @@ function generatePoem(event) {
 
     let apiKey = "e1c36520c14f56fa74b8fob3tcc313d4";
     let prompt = "Please generate a poem for the chosen topic";
-    let context = "You are a poem generator, please generate a short 2 verse poem."
+    let context = "You are a poem generator, please generate a short 2 verse poem.";
     let apiUrl =  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(typePoem);
 }
 
-let form = document.querySelectorAll("#form");
+let form = document.querySelector("#form");
 form.addEventListener("submit", generatePoem);
-
