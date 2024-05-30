f4646fd14bc0550c55a132afeef0679af0f0fcdd
radevden <radevden@gmail.com>
2024-05-30 14:20:27 +0200

    changed the topic + form

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ function generatePoem(event) {
 
     let topicInput = document.querySelector("#topic");
     let apiKey = "e1c36520c14f56fa74b8fob3tcc313d4";
-    let prompt =`Please generate a poem about ${topicInput.value}`;
-    let context = "You are a poem generator, please generate a short 2 verse poem in basic HTML and separate each line. Please include a title.";
+    let prompt =`Please generate a trip iterinary to ${topicInput.value}`;
+    let context = "You are a vacation iterinary generator, please generate an itinerary of interesting places and places worth seeing day by day in basic HTML and separate each line. Please include a title.";
     let apiUrl =  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let poemElement = document.querySelector(".pPoem");
     poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `<div class="loader"></div> Generating a poem about ${topicInput.value}...`;
+    poemElement.innerHTML = `<div class="loader"></div> Generating an itinerary for ${topicInput.value}...`;
 
     axios.get(apiUrl).then(typePoem);
 }
